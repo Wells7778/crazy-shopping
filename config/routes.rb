b9ce686015c0ecce70f39a8852e09f035e088774
Wells <chawkeye@gmail.com>
2018-03-01 13:10:33 +0800

    setup routes for vistor and admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :products
+
+    root "products#index"
+  end
+
+  resources :products, only: [:index, :show]
+
+  root "products#index"
 end
