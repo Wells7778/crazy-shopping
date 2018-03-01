de262d0dc1f5ae5012524eb9017958a50499be8e
Wells <chawkeye@gmail.com>
2018-03-01 17:24:40 +0800

    change remove from cart to ajax

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,8 +24,11 @@ class ProductsController < ApplicationController
 
   def remove_from_cart
     @product = Product.find(params[:id])
+    @cart_item = current_cart.cart_items.find_by(product_id: @product)
     current_cart.remove_cart_item(@product)
 
-    redirect_to root_path
+    respond_to do |format|
+      format.js
+    end
   end
 end
