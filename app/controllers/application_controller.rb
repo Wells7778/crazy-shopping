067190d136940205c8369b7a169efde8954e3ec3
Wells <chawkeye@gmail.com>
2018-03-01 14:44:56 +0800

    user can added product to cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_cart
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  private
+  def current_cart
+    @cart || set_cart
+  end
+
+  def set_cart
+    if sessioin[:cart_id]
+      @cart = Cart.find_by(id: sessioin[:cart_id])
+    end
+
+    @cart ||= Cart.create
+
+    sessioin[:cart_id] = @cart.id
+    @cart
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,11 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    current_cart.add_cart_item(@product)
+
+    redirect_to root_path
+  end
 end
