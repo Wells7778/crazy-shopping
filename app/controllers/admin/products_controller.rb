ab9e9408e4bd3b0c8400f020fa4bae3019e24dcc
Wells <chawkeye@gmail.com>
2018-03-06 10:09:18 +0800

    style admin products index page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < Admin::BaseController
   def index
-    @products = Product.page(params[:page]).per(30)
+    @products = Product.page(params[:page]).per(20)
   end
 
 end
