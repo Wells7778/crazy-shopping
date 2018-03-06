5024b93e1f30efa0438d1a6556b12e6bc2b3d715
Wells <chawkeye@gmail.com>
2018-03-06 11:59:37 +0800

    setup routes for admin::order

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   namespace :admin do
     resources :products
-
+    resources :orders, except: [:new, :create]
     root "products#index"
   end
 
