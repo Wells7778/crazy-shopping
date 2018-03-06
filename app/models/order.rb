4ca80f69cba2b84d2acb1c40d547c8340ab05f48
Wells <chawkeye@gmail.com>
2018-03-06 14:50:47 +0800

    added orders#create

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,10 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :products, through: :order_items
   belongs_to :user
+
+  def add_cart_items_to_order_items(cart)
+    cart.cart_items.each do |item|
+      self.order_items.create(quantity: item.quantity, price: item.product.price, product_id: item.product.id)
+    end
+  end
 end
