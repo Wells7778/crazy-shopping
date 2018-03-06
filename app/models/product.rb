11b82872724b3f466cb49f7a9946a2ae13fa7ab1
Wells <chawkeye@gmail.com>
2018-03-06 11:59:07 +0800

    set association

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates_presence_of :name, :description, :price
   has_many :cart_items, dependent: :destroy
   has_many :carts, through: :cart_items
+  has_many :order_items
+  has_many :orders, through: :order_items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
+  has_many :orders
+
   def is_admin?
     self.role == "admin"
   end
