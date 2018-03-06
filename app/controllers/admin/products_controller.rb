da864ea7420951772e41ae5eaa9a4c2933b14a5e
Wells <chawkeye@gmail.com>
2018-03-06 10:44:03 +0800

    added admin::products#edit & #update

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < Admin::BaseController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   def index
     @products = Product.page(params[:page]).per(20)
   end
@@ -10,9 +11,19 @@ class Admin::ProductsController < Admin::BaseController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to admin_root_path, notice: "added product successfully"
+      redirect_to admin_root_path, notice: "add product successfully"
     else
-      render :new, alert: "something wrong!!"
+      flash[:alert] =  "something wrong!!"
+      render :new
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_root_path, notice: "update product successfully"
+    else
+      flash[:alert] =  "something wrong!!"
+      render :edit
     end
   end
 
@@ -21,5 +32,9 @@ class Admin::ProductsController < Admin::BaseController
 
   def product_params
     params.require(:product).permit(:name, :image, :description, :price)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
