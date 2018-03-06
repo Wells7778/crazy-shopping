199fd2451c113a1e109bc4cffc4e27ada45ce11a
Wells <chawkeye@gmail.com>
2018-03-06 10:38:55 +0800

    admin::products#new & #create

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,4 +3,23 @@ class Admin::ProductsController < Admin::BaseController
     @products = Product.page(params[:page]).per(20)
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_root_path, notice: "added product successfully"
+    else
+      render :new, alert: "something wrong!!"
+    end
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :image, :description, :price)
+  end
 end
